73cf9786cb5309bfd4b191c5e707ddae937d2411
Joshua Williams <jmw0426@hotmail.com>
2020-05-05 15:29:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,13 +12,11 @@ def line(katz_deli)
 end    
 
 def take_a_number(katz_deli, name)
-  # add_person = []
-  
-  # katz_deli.each_with_index{|person, number| add_person << "#{person}"}
   katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
 
+def now_serving()
   
   
   
